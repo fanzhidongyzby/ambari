41c839715f1e51b22b91c85bf6eb83309159c0ee
docker_common <docker_common@TENCENT64.site>
2015-07-28 16:34:31 +0800

    add sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+#for i in `find . -type f -name "*ambari*" -or -name "*.py" -or -name "*.sh" -or -name "*.java" |grep -v .git | grep -v target |grep -v jar|grep -v png`;do dos2unix $i; done
+
+mvn versions:set -DnewVersion=2.2.0.0
+pushd ambari-metrics
+mvn versions:set -DnewVersion=2.2.0.0
+popd
+mvn install package rpm:rpm -Dfindbugs.skip -DskipTests -Drat.skip -Dmaven.test.skip -DskipAssembly -DnewVersion=2.2.0.0 -Dpython.ver="python >= 2.6" -X
