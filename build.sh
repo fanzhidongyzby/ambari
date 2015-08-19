222d399ca5fce6c87558f923f25e27db2ecb89e5
florianfan <florianfan@tencent.com>
2015-08-19 12:01:51 +0800

    modify build sh

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 
 #for i in `find . -type f -name "*ambari*" -or -name "*.py" -or -name "*.sh" -or -name "*.java" |grep -v .git | grep -v target |grep -v jar|grep -v png`;do dos2unix $i; done
+mkdir ambari-agent/target/ambari-agent-2.1.1.0/ambari_agent
+mkdir ambari-client/python-client/target/python-client-2.1.1.0/ambari_client
+mkdir ambari-shell/ambari-python-shell/target/ambari-python-shell-2.1.1.0/ambari_shell
 
 rm -rf ambari-agent/target/ambari-agent-2.1.1.0/ambari_agent/*
 cp -r ambari-agent/src/main/python/ambari_agent/* ambari-agent/target/ambari-agent-2.1.1.0/ambari_agent
@@ -11,3 +14,4 @@ pushd ambari-metrics
 mvn versions:set -DnewVersion=2.1.1.0
 popd
 mvn install package rpm:rpm -Dfindbugs.skip -DskipTests -Drat.skip -Dmaven.test.skip -DskipAssembly -DnewVersion=2.1.1.0 -Dpython.ver="python >= 2.6" -X
+# mvn install -pl ambari-agent package rpm:rpm -Dfindbugs.skip -DskipTests -Drat.skip -Dmaven.test.skip -DskipAssembly -DnewVersion=2.1.1.0 -Dpython.ver="python >= 2.6" -X
--- start-build.sh
+++ start-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+BUILD_DIR=`pwd`
 
 export AMBARI_BUILD_HOME=/data/home/docker_common/jerryjzhang/ambari-build
 
-echo "Building $1"
+echo "Building $BUILD_DIR"
 
-docker run -it --rm -v $1:/ambari \
+docker run -it --rm -v $BUILD_DIR:/ambari \
     -v $AMBARI_BUILD_HOME/m2:/root/.m2 \
     -v $AMBARI_BUILD_HOME/ambari-web/node_modules:/ambari/ambari-web/node_modules \
     -v $AMBARI_BUILD_HOME/ambari-admin/node_modules:/ambari/ambari-admin/src/main/resources/ui/admin-web/node_modules \
