379f50bd6b539626137fed658928f3e773cf5e90
florianfan <florianfan@tencent.com>
2015-10-23 10:16:09 +0800

    merge svn

--- ambari-server/src/main/resources/scripts/bootstrap_agent_env_setup.sh
+++ ambari-server/src/main/resources/scripts/bootstrap_agent_env_setup.sh
@@ -26,6 +26,12 @@ if [ $? -ne 0 ];then
   echo "[===== BOOTSTRAP-ERROR =====]copy the bootstrap_agent_setup.tar.gz to ${hostIP} failed"
   exit -1
 fi
+#check the tty
+if [ "${sshUser}" == "root" ]
+  then 
+    ttyCheck="chmod 755 /etc/sudoers;sed -i 's/[#]\{0,\}Defaults *requiretty/#Defaults requiretty/g' /etc/sudoers;chmod 440 /etc/sudoers"
+    ${currentPath}/execRemoteCmd.exp ${hostIP} ${sshUser} ${sshPass} 150 "${ttyCheck}"
+fi
 remoteCmd="cd /tmp;sudo tar -xzvf bootstrap_agent_setup.tar.gz >/dev/null;sudo rm -f bootstrap_agent_setup.tar.gz;sudo chmod 777 bootstrap_agent_env_script.sh;sudo ./bootstrap_agent_env_script.sh"
 ${currentPath}/execRemoteCmd.exp ${hostIP} ${sshUser} ${sshPass} 150 "${remoteCmd}"
 if [ $? -ne 0 ];then
